1ec71ff0d3e3a93a1cbb905e8becce6e1a68be1f
Gekko <evgeni.zhukov@gmail.com>
2019-03-31 07:18:24 +0300

    Добавил ветвление с несколькими вариантами ответов

--- new_program.py
+++ new_program.py
@@ -3,3 +3,11 @@ print("Привет программист!")
 
 name = input("Введите ваше имя: ")
 print(name, ", добро пожаловать!")
+
+inputUser = input("Давайте поработаем? (Y/N)")
+if inputUser == 'y':
+    print("Это хорошо")
+elif inputUser == 'n':
+    print("До новых встреч")
+else:
+    print("Неверный ввод")
