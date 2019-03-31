025c80566904d4eb133413b0a5cab13d2eecf95b
Gekko <evgeni.zhukov@gmail.com>
2019-03-31 07:46:32 +0300

    Добавил возможность вывести список запущенных процессов и список файлов текущей директории.

--- new_program.py
+++ new_program.py
@@ -1,3 +1,6 @@
+import os
+import psutil
+
 print("Это моя первая программа")
 print("Привет программист!")
 
@@ -6,7 +9,19 @@ print(name, ", добро пожаловать!")
 
 inputUser = input("Давайте поработаем? (Y/N)")
 if inputUser == 'y':
-    print("Это хорошо")
+    print("Список возможностей:")
+    print("  [1] - вывести список файлов текущей директории")
+    print("  [2] - вывести информацию о системе")
+    print("  [3] - показать список запущенных процессов")
+    inputUser = int(input("Выберите возможность: "))
+    if inputUser == 1:
+        print(os.listdir())
+    elif inputUser == 2:
+        pass
+    elif inputUser == 3:
+        print(psutil.pids())
+    else:
+        print("Неверный ввод")
 elif inputUser == 'n':
     print("До новых встреч")
 else:
