fed2e6e35c551af7734533cd1cac656562bd8308
Gekko58 <evgeni.zhukov@gmail.com>
2019-04-02 11:38:33 +0300

    Добавил создание дубликатов файлов в текущей директории

--- new_program.py
+++ new_program.py
@@ -1,6 +1,7 @@
 import os
 import psutil
 import sys
+import shutil
 
 print("Это моя первая программа")
 print("Привет программист!")
@@ -15,6 +16,7 @@ if inputUser == 'y':
         print("  [1] - вывести список файлов текущей директории")
         print("  [2] - вывести информацию о системе")
         print("  [3] - показать список запущенных процессов")
+        print("  [4] - дублирование файлов в текущей директории")
         print("  [q] - выход")
         inputUser = input("Выберите возможность: ")
         if inputUser == '1':
@@ -29,6 +31,12 @@ if inputUser == 'y':
             print("Список процессов: ", psutil.pids())
         elif inputUser == 'q':
             print("До новых встреч")
+        elif inputUser == '4':
+            file_list = os.listdir()
+            for i in range(0, len(file_list), 1): #Получаем список файлов и папкок
+                if os.path.isfile(file_list[i]): #Проверяем, что это файл
+                    new_name_file = file_list[i] + ".dupl" #Формируем новое имя
+                    shutil.copy(file_list[i], new_name_file) #Копируем
         else:
             print("Неверный ввод")
 elif inputUser == 'n' or inputUser == 'q':
